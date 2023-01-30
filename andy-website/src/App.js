cbb71dd9bdbf39bc70f0ba97ec5fec2015741af7
Andy <andyvu04@gmail.com>
2023-01-30 01:55:27 -0600

    made home section pretty

--- andy-website/src/App.js
+++ andy-website/src/App.js
@@ -4,12 +4,14 @@ import Navbar from './Navbar.js';
 function App() {
   return (
     <div className='App'>
+      <img className='bg' src='northwestern-background.jpeg' alt='background' />
       <Navbar />
-      <header className='App-header'>
+      <div className='home' >
         <h1>Hello, I'm Andy!</h1>
         <h4>Software Engineer at Northwestern</h4>
-      </header>
+      </div>
     </div>
+    
   );
 }
 
--- andy-website/src/Navbar.js
+++ andy-website/src/Navbar.js
@@ -4,10 +4,12 @@ import logo from './logo.svg';
 function Navbar() {
     return(
         <div className='Navbar'>
-            <a className='active'>Home</a>
-            <a>About Me</a>
-            <a>My Skills</a>
-            <a>Contacts</a>
+            <ul className='links'>
+                <a className='active'><strong>Home</strong></a>
+                <a><strong>About Me</strong></a>
+                <a><strong>My Skills</strong></a>
+                <a><strong>Contacts</strong></a>
+            </ul>
             <img className='logo' src={logo} alt='logo' />
         </div>
     )
