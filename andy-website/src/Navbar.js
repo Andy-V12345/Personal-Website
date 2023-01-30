d5b6bf0301ff746a59626d43cf15d05187f2c35a
Andy <andyvu04@gmail.com>
2023-01-30 02:50:55 -0600

    added toggle feature to navbar

--- andy-website/src/Navbar.js
+++ andy-website/src/Navbar.js
@@ -1,14 +1,37 @@
 import './App.css';
 import logo from './logo.svg';
+import React from 'react';
 
 function Navbar() {
+    const activeStates = [
+        {'name': 'Home', 'isActive': true},
+        {'name': 'About Me', 'isActive': false},
+        {'name': 'My Skills', 'isActive': false},
+        {'name': 'Contacts', 'isActive': false}
+    ]
+    const [actives, setActive] = React.useState(activeStates)
+
+    function changeStates(index) {
+        for (let i = 0; i < activeStates.length; i++) {
+            if (activeStates[i].isActive) {
+                activeStates[i].isActive = false
+            }
+        }
+        activeStates[index].isActive = true;
+        setActive(activeStates)
+    }
+
+
     return(
         <div className='Navbar'>
             <ul className='links'>
-                <a className='active'><strong>Home</strong></a>
-                <a><strong>About Me</strong></a>
-                <a><strong>My Skills</strong></a>
-                <a><strong>Contacts</strong></a>
+                {actives.map((active, index) => (
+                    <li key={active.name}>
+                        <a onClick={() => changeStates(index)} className={active.isActive ? 'active' : ''}>
+                            <strong>{active.name}</strong>
+                        </a>
+                    </li>
+                ))}
             </ul>
             <img className='logo' src={logo} alt='logo' />
         </div>
